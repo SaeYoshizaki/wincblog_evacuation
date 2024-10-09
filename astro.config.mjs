bcef1fc54fb3cf99ee8a0758b9662cf062087c81
SaeYoshizaki <yoshizakisae@gmail.com>
2024-10-09 10:13:32 +0900

    fiing errors

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import dotenv from 'dotenv';
 
-// https://astro.build/config
+// .envファイルを読み込む
+dotenv.config();
+
 export default defineConfig({
   integrations: [react()],
   vite: {
--- src/libs/micro.ts
+++ src/libs/micro.ts
@@ -1,9 +1,10 @@
-import { createClient, type MicroCMSQueries } from "microcms-js-sdk";
+import { createClient, type MicroCMSQueries  } from 'microcms-js-sdk';
 
 const client = createClient({
-    serviceDomain: import.meta.env.SERVICE_DOMAIN,
-    apiKey: import.meta.env.API_KEY,
+  serviceDomain: import.meta.env.MICROCMS_SERVICE_DOMAIN,  // サービスドメインを環境変数から取得
+  apiKey: import.meta.env.MICROCMS_API_KEY,               // APIキーを環境変数から取得
 });
+
 
 export const getUsers = async (queries: MicroCMSQueries) => {
     return await client.get({
